59aee2ac631a45e6d783c3372996e49bb4782e56
hoang-debug <campus199@gmail.com>
2021-06-14 14:54:57 +0700

    chua hoan thanh

---
+++ Day 9/Bai1/Bai1.js
@@ -0,0 +1,7 @@
+let i = 0;
+    function buttonPlusClick() {
+        document.getElementById('count').value = ++i;
+    }
+    function buttonMinusClick(){
+        document.getElementById('count').value = --i;
+    }
---
+++ Day 9/Bai2/Bai2.js
@@ -0,0 +1,3 @@
+
+let setCountVal = document.getElementById('setCount').value;
+
